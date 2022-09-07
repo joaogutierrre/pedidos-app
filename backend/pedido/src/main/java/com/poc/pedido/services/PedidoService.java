b77f0c71e47556432b81c593a18df802abd9675b
João Gutierre <gutierrejoao@gmail.com>
2022-09-06 23:57:21 -0300

    services/PedidoService.java

---
+++ backend/pedido/src/main/java/com/poc/pedido/services/PedidoService.java
@@ -0,0 +1,41 @@
+package com.poc.pedido.services;
+
+import com.poc.pedido.entities.PedidoModel;
+import com.poc.pedido.repositories.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PedidoService {
+    final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public List<PedidoModel> findAllPedidoModels() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<PedidoModel> findPedidoById(UUID id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public boolean existsByCpf(String cpf) {
+        return pedidoRepository.existsByCpf(cpf);
+    }
+
+    public boolean existsBydatapedido(LocalDateTime datapedido) {
+        return pedidoRepository.existsBydatapedido(datapedido);
+    }
+
+    @Transactional
+    public PedidoModel save(PedidoModel pedidoModel) {
+        return pedidoRepository.save(pedidoModel);
+    }
+}
